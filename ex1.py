bba55af70ddbf5a33fd3e5f31840c84eda4f7b11
IsaiasMart <isaiasmcald@gmail.com>
2023-10-20 14:15:09 +0200

    ejercicios

--- ex1.py
+++ ex1.py
@@ -6,11 +6,12 @@ Exercices for input
 name = input("What is your name? ")
 
 # 2. Ask the user for his/her age
-
+age=int(input("how old are you?"))
 # 3. Ask the user for his/her favorite_color
-
+fav_color = input("What is your favourite color? ")
 # 4. Ask the user for his/her favorite_number
-
+fav_number=int(input("what's uour favourite number?"))
 # 5. Print out the information in a sentence
-
+print(f"this is the information you just gave: name: {name}, age: {age}, favourite color and number: {fav_color} and {fav_number} ")
 # 6. Multiply the user's favorite number by 10 and print out the result
+print(f"this would be your age if multipliyed by 10: {10*age}")
--- ex3.py
+++ ex3.py
@@ -10,10 +10,11 @@ rps = ['rock','paper','scissors']
 
 # 2. Create a variable called user_choice that takes input from the user and stores it.
 #   Make sure the user enters rock, paper, or scissors.
-user_choice = "" # fix this, user input, hint: use input("...")
+user_choice = input("choose rock paper or scissors to play!")
+ # fix this, user input, hint: use input("...")
 
 # 3. Create a variable called computer_choice that randomly chooses from rps.
-computer_choice = "" # fix this, random choice from rps
+computer_choice = random.choice(rps) # fix this, random choice from rps
 
 # 4. Print out the user_choice and the computer_choice.
 print("You chose: " + user_choice)
@@ -24,10 +25,21 @@ print("The computer chose: " + computer_choice)
 if user_choice == computer_choice:
   print("It's a tie!")
 # write stuff here
-
 # rock beats scissors
+if user_choice =="rock" and computer_choice =="scissors":
+  print("you won user!")
+elif user_choice =="scissors" and computer_choice =="rock":
+  print("you loose user!")
 # paper beats rock
+if user_choice =="paper" and computer_choice =="rock":
+  print("you won user!")
+elif user_choice =="rock" and computer_choice =="paper":
+  print("you loose user!")
 # scissors beats paper
+if user_choice =="scissors" and computer_choice =="paper":
+  print("you won user!")
+elif user_choice =="paper" and computer_choice =="scissors":
+  print("you loose user!")
 # hint: use elif
 # hint: use and or not == !=
 # hint: use "It's a tie!"
